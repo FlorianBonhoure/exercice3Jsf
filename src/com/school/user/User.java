165704e4b9f2d685f2a8a1eb86ebf61c4da9e54a
Florian Bonhoure <florian.b47@hotmail.fr>
2022-11-08 14:26:08 +0100

    add other fields

--- src/com/school/user/User.java
+++ src/com/school/user/User.java
@@ -1,13 +1,20 @@
 package com.school.user;
 
-import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
 @ManagedBean(name="user")
+
 public class User implements Serializable {
 	private static final long serialVersionUID= 1L;
 	private String nom;
 	private String prenom;
+	private String mdp;
+	private boolean marie;
+	private String sexe;
+	
 	
 	public void User() {
 		System.out.println("Constructeur user");
@@ -29,9 +36,46 @@ public class User implements Serializable {
 	}
 
 	public void setPrenom(String prenom) {
-		System.out.println("getPrenom");
+		System.out.println("setPrenom");
 		this.prenom = prenom;
 	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public boolean isMarie() {
+		return marie;
+	}
+
+	public void setMarie(boolean estMarie) {
+		this.marie = estMarie;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
 	
+	public void afficherInfo() {
+		String situation;
+		if(marie) {
+			if(sexe.equals("homme")) {
+				situation="marié";
+			} else {
+				situation="mariée";
+			}
+		} else {
+			situation="celibataire";
+		}
+		System.out.printf("Bonjour %s %s, vous etes un(e) %s %s\n",prenom,nom,sexe,situation);
+	}
 	
 }
