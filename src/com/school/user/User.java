d31f7a4d349f112034c50e626453f1e496e47a4e
Florian Bonhoure <florian.b47@hotmail.fr>
2022-11-08 11:45:10 +0100

    add user bean

--- src/com/school/user/User.java
+++ src/com/school/user/User.java
@@ -1,5 +1,37 @@
 package com.school.user;
 
-public class User {
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
 
+@ManagedBean(name="user")
+public class User implements Serializable {
+	private static final long serialVersionUID= 1L;
+	private String nom;
+	private String prenom;
+	
+	public void User() {
+		System.out.println("Constructeur user");
+	}
+
+	public String getNom() {
+		System.out.println("getNom");
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		System.out.println("setNom");
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		System.out.println("getPrenom");
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		System.out.println("getPrenom");
+		this.prenom = prenom;
+	}
+	
+	
 }
